ba62aabb83827637a37f8cdf74165e35767c14b6
Elliott Minns <elliott.minns@pm.me>
2022-05-23 11:19:34 -0500

    remove autocmd to cwd in NvimTree

--- .config/nvim/lua/custom/autocmds.lua
+++ .config/nvim/lua/custom/autocmds.lua
@@ -1,14 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
 
--- open nvim with a dir while still lazy loading nvimtree
-autocmd("BufEnter", {
-  callback = function()
-    if vim.api.nvim_buf_get_option(0, "buftype") ~= "terminal" then
-      vim.cmd "lcd %:p:h"
-    end
-  end,
-})
-
 -- Enable spellchecking in markdown, text and gitcommit files
 autocmd("FileType", {
   pattern = { "gitcommit", "markdown", "text" },
