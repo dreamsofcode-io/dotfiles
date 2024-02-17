49cff26b9913b4ef60c8b66b12ce20e4c6201869
Elliott Minns <elliott.minns@pm.me>
2024-02-16 22:50:33 -0600

    add in telescope file browser

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -9,19 +9,55 @@ return {
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>fn', ":Telescope file_browser path=%:p:h select_buffer=true<CR>", {})
     end,
-    --opts = function()
-      --return require "plugins.configs.telescope"
-    --end,
+    opts = function()
+      return {
+        defaults = {
+          vimgrep_arguments = {
+            "rg",
+            "-L",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+          },
+        },
+        extensions = {
+          file_browser = {
+            theme = "ivy",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+              ["i"] = {
+                -- your custom insert mode mappings
+              },
+              ["n"] = {
+                -- your custom normal mode mappings
+              },
+            },
+          },
+        },
+        extensions_list = {
+          "file_browser",
+        },
+      }
+    end,
     config = function(_, opts)
       local telescope = require "telescope"
       telescope.setup(opts)
 
       -- load extensions
-      --for _, ext in ipairs(opts.extensions_list) do
-        --telescope.load_extension(ext)
-      --end
+      for _, ext in ipairs(opts.extensions_list) do
+        telescope.load_extension(ext)
+      end
     end,
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
   },
   -- Only load whichkey after all the gui
   {
