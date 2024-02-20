c48a0618d2fe7a23433710084730118e100982b1
Elliott Minns <elliott.minns@pm.me>
2024-02-20 15:10:16 -0600

    line trimming and better tab spacing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,6 +22,11 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
--- .config/nvim/lua/plugins/edit.lua
+++ .config/nvim/lua/plugins/edit.lua
@@ -1,3 +1,13 @@
 return {
-	{ "tpope/vim-sleuth" },
+	{
+		"tpope/vim-sleuth",
+		event = "VeryLazy",
+	},
+	{
+		"cappyzawa/trim.nvim",
+		event = "VeryLazy",
+		opts = {
+			ft_blocklist = {"markdown"},
+		},
+	},
 }
