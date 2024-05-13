d02295858c06082cca639ec3f7f1133069304aa3
Elliott Minns <elliott.minns@pm.me>
2024-05-12 20:03:09 -0500

    set homemanager to 23.11

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,7 +13,7 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
     # Home Manager
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Alacritty theme
